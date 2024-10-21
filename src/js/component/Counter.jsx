57a1a424512a9bf54cecf2d9476c53f8cada3a66
Jose Pindado <joseangelpindado@gmail.com>
2024-10-21 19:30:30 +0000

    Second commit

--- src/js/component/Counter.jsx
+++ src/js/component/Counter.jsx
@@ -1,52 +1,30 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
- const Counter = () => {
-    let initialCounter = 0;
-
-    const updateCounter = () => {
-        
-        const six = Math.floor(initialCounter / 100000) % 10;
-        const five = Math.floor(initialCounter / 10000) % 10;
-        const four = Math.floor(initialCounter / 1000) % 10;
-        const three = Math.floor(initialCounter / 100) % 10;
-        const two = Math.floor(initialCounter / 10) % 10;
-        const one = Math.floor(initialCounter / 1) % 10;
-
-        document.querySelector(".six").textContent = six;
-        document.querySelector(".five").textContent = five;
-        document.querySelector(".four").textContent = four;
-        document.querySelector(".three").textContent = three;
-        document.querySelector(".two").textContent = two;
-        document.querySelector(".one").textContent = one;
-
-        initialCounter++;
-    };
-
-    setInterval(updateCounter, 1000);
+ const Counter = (props) => {
 
     return (
-        <div className="totalCounter">
+       <div className="totalCounter">
             <div className="calendar">
                 <i className="far fa-clock" />
             </div>
-            <div className="six">0</div>
-            <div className="five">0</div>
-            <div className="four">0</div>
-            <div className="three">0</div>
-            <div className="two">0</div>
-            <div className="one">0</div>
+            <div className="six">{props.six}</div>
+            <div className="five">{props.five}</div>
+            <div className="four">{props.four}</div>
+            <div className="three">{props.three}</div>
+            <div className="two">{props.two}</div>
+            <div className="one">{props.one}</div>
         </div>
     );
 };
 
-// Counter.propTypes = {
-//     six: PropTypes.number,
-//     five: PropTypes.number,
-//     four: PropTypes.number,
-//     three: PropTypes.number,
-//     two: PropTypes.number,
-//     one: PropTypes.number,
-// };
+        Counter.propTypes = {
+            six: PropTypes.number.isRequired,
+            five: PropTypes.number.isRequired,
+            four: PropTypes.number.isRequired,
+            three: PropTypes.number.isRequired,
+            two: PropTypes.number.isRequired,
+            one: PropTypes.number.isRequired,
+        };
 
 export default Counter;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import Counter from "./Counter.jsx";
+import PropTypes from "prop-types";
 
 
 //create your first component
 const Home = () => {
+	
+	const [counter, setCounter] = useState (0)
+
+	useEffect(()=>{
+		const interval = setInterval(()=> {
+			setCounter ((prevCounter)=>prevCounter+1)
+		},1000)
+	},[])
+
+	const six = Math.floor(counter / 100000) % 10;
+    const five = Math.floor(counter / 10000) % 10;
+    const four = Math.floor(counter / 1000) % 10;
+    const three = Math.floor(counter / 100) % 10;
+    const two = Math.floor(counter / 10) % 10;
+    const one = Math.floor(counter / 1) % 10;
+
 	return (
-		<>
-			<Counter/>
-		</>
+			<Counter
+			six = {six}
+			five = {five}
+			four = {four}
+			three = {three}
+			two = {two}
+			one = {one}
+			/>
 	);
 };
 
